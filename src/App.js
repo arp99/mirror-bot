2026b35aacc387b4ae33a533a2e784233a23ef84
Arpan Mondal <arpan19991025@gmail.com>
2022-03-03 10:40:01 +0530

    Update: import and use components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+import ChatInput from "./components/inputBox/InputBox";
+import ChatBubble from "./components/ChatBubble/ChatBubble";
+import "./App.css";
+import { useChat } from "./Context/ChatContext";
 
 function App() {
+  const { chats } = useChat();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="h-[90%] w-[70%] mx-auto border-2 border-solid border-blue-500">
+        <div className="h-[90%] overflow-y-scroll">
+          {chats.length === 0 ? (
+            <p>No chat History</p>
+          ) : (
+            chats.map(({ message }) => (
+              <>
+                <ChatBubble sentBy={"user"}>{message}</ChatBubble>
+                <ChatBubble sentBy={"bot"}>{message}</ChatBubble>
+              </>
+            ))
+          )}
+        </div>
+        <ChatInput />
+      </div>
     </div>
   );
 }
